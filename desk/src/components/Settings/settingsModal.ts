a8ad13608ff9bbc31cd0a38ceb08914ba2a860eb
Pratik <badhepd@gmail.com>
2025-07-22 19:24:46 +0530

    wip

--- desk/src/components/Settings/settingsModal.ts
+++ desk/src/components/Settings/settingsModal.ts
@@ -1,4 +1,4 @@
-import { markRaw, ref } from "vue";
+import { h, markRaw, ref } from "vue";
 import ImageUp from "~icons/lucide/image-up";
 import LucideMail from "~icons/lucide/mail";
 import LucideUser from "~icons/lucide/user";
@@ -11,6 +11,7 @@ import EmailConfig from "./EmailConfig.vue";
 import TeamsConfig from "./Teams/TeamsConfig.vue";
 import Sla from "./Sla/Sla.vue";
 import HolidayList from "./Holiday/Holiday.vue";
+import Settings from "~icons/lucide/settings-2";
 
 export const tabs = [
   {
@@ -41,6 +42,11 @@ export const tabs = [
   {
     label: "Business Holidays",
     icon: markRaw(Briefcase),
+    component: markRaw(HolidayList),
+  },
+  {
+    label: "Assignment Rules",
+    icon: markRaw(h(Settings, { class: "rotate-90" })),
     component: markRaw(HolidayList),
   },
 ];
